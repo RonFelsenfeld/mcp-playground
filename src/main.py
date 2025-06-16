6d241c1832ce42cca8ab675d31f81ae30af06ac5
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2025-06-16 10:53:54 +0300

    feat(app) - remove fastapi

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from src.api.routes.news_routes import news_router
-
-API_PREFIX = "/api"
-
-app = FastAPI()
-
-app.include_router(prefix=API_PREFIX, router=news_router)
