1eeae033c275ad78bb974c6ad89379f8749c9e1f
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2025-06-16 11:57:07 +0300

    chore - add source link to example server

--- src/__init__.py
+++
@@ -1,2 +0,0 @@
-# This MCP server is a simple example of how to use the MCP framework to create a server.
-# Source - https://modelcontextprotocol.io/quickstart/server
---
+++ src/example_weather_mcp/__init__.py
@@ -0,0 +1,2 @@
+# This MCP server is a simple example of how to use the MCP framework to create a server.
+# Source - https://modelcontextprotocol.io/quickstart/server
